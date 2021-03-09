1f6bf7275d52a89beae80f8f065e8f7c1ab61c51
Vanshika-Jathar <vanshikahjathar@gmail.com>
2021-03-09 22:44:26 +0530

    webview

---
+++ app/src/main/java/com/vanshika/p5/MainActivity.java
@@ -0,0 +1,27 @@
+package com.vanshika.p5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+private ImageView b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (ImageView) findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
